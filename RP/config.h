c286d06520852f20861045a4b618f2088b68d2d3
kumakichi <49866621+137900114@users.noreply.github.com>
2019-11-20 10:20:47 +0800

    2

---
+++ RP/config.h
@@ -0,0 +1,17 @@
+#pragma once
+//#define G_DEBUG
+
+#include "..//..//Common/PNGImage.h"
+#include "../../Common/tgaimage.h"
+#include "../../Common/geometry.h"
+#include "../../Common/Model.h"
+#include "../../Common/Camera.h"
+#include <iostream>
+using namespace std;
+
+const Color WIHTE = {255,255,255,255};
+const Color RED = { 255,0,0,255 };
+const Color GREEN = { 0,225,0,255 };
+const Color BLUE = { 0,0,255,255 };
+const Color BLACK = { 0,0,0,255 };
+
